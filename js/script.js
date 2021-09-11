51b1d9432cca3c33805b48a173402192e1fcea03
dherrerab-89 <dherrerab89@gmail.com>
2021-09-11 15:24:58 -0300

    arreglo en body
    solucion de problema

--- js/script.js
+++ js/script.js
@@ -1,2 +1,17 @@
+function alerta(){
 const nombre=prompt("Introduce tu nombre")
 window.alert(`Bienvenid@ a la PTU feliz, ${nombre}`)
+}
+
+function generarNuevoColor(){
+	var simbolos, color;
+	simbolos = "0123456789ABCDEF";
+	color = "#";
+
+	for(var i = 0; i < 6; i++){
+		color = color + simbolos[Math.floor(Math.random() * 16)];
+	}
+
+	document.body.style.background = color;
+	document.getElementById("hexadecimal").innerHTML = color;
+}
