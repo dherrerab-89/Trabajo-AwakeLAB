e7384291dc4b9b884871f0835d5a7933797e6675
dherrerab-89 <dherrerab89@gmail.com>
2021-09-13 19:56:25 -0300

    Update script.js
    Se corrige error

--- js/script.js
+++ js/script.js
@@ -1,21 +1,22 @@
 const nombre=prompt("Introduce tu nombre")
-window.alert(`Bienvenid@ a la PTU feliz, ${nombre}`)
-const nombre = prompt('cual es tu nombre')
+window.alert(`Bienvenid@ a la PTU feliz, ${nombre}`);
+
+
 if(nombre === null){
-nombre = ' ';
-}
+    nombre = ' ';
+    }
 
-
-function newColor(){
-	var simbolos, color;
-	simbolos = "0123456789ABCDEF";
-	color = "#";
-
-	for(var i = 0; i < 6; i++){
-		color = color + simbolos[Math.floor(Math.random() * 16)];
-	}
-
-	document.body.style.background = color;
-	document.getElementById("hexadecimal").innerHTML = color;
-}
-
+    function newColor(){
+        var simbolos, color;
+        simbolos = "0123456789ABCDEF";
+        color = "#";
+    
+        for(var i = 0; i < 6; i++){
+            color = color + simbolos[Math.floor(Math.random() * 16)];
+        }
+    
+        document.body.style.background = color;
+        document.getElementById("hexadecimal").innerHTML = color;
+    }
+    
+    
